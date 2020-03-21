e5d3931377ae0e789e5c8c7cfa3477a5a8c6cc9b
Actireon <eheieiei37@gmail.com>
2020-03-21 18:08:12 +0200

    Updates

---
+++ precache-manifest.7440887d2e4a849f77c0a7195fbbd6ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98b462c925abf43d076473fc6f2e500",
+    "url": "/CyberSportPlatform/index.html"
+  },
+  {
+    "revision": "b1ba91fad268c5d6070f",
+    "url": "/CyberSportPlatform/static/css/main.ec35e3ad.chunk.css"
+  },
+  {
+    "revision": "d719627768f8b344a4dc",
+    "url": "/CyberSportPlatform/static/js/2.533aab41.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/CyberSportPlatform/static/js/2.533aab41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ba91fad268c5d6070f",
+    "url": "/CyberSportPlatform/static/js/main.ec15478c.chunk.js"
+  },
+  {
+    "revision": "fcf53ac81f323e25999e",
+    "url": "/CyberSportPlatform/static/js/runtime-main.c928e4c5.js"
+  },
+  {
+    "revision": "400c697f4ab4f776b621fb351b662500",
+    "url": "/CyberSportPlatform/static/media/card-star-silver.400c697f.png"
+  },
+  {
+    "revision": "d267a247bcefd4ef99288c23915b8166",
+    "url": "/CyberSportPlatform/static/media/ggbet.d267a247.svg"
+  },
+  {
+    "revision": "4ee68a421022d4080aed2666637b7ef1",
+    "url": "/CyberSportPlatform/static/media/header-bg.4ee68a42.png"
+  },
+  {
+    "revision": "4e6c83301c03f4c08f7cb4f04237170b",
+    "url": "/CyberSportPlatform/static/media/header-logo.4e6c8330.svg"
+  }
+]);
